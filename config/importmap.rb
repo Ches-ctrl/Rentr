2ae557c22bd23378bf856aeb855c2f92ec8986a5
Direncan Kaygisiz <direncan89@gmail.com>
2023-11-21 16:18:41 +0000

    added date pickr and image

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
